ef56d4b80b18c9f5bdd3b7bdeb06221adc2b6f65
BblakEe <buihuyduc111@gmail.com>
2023-05-22 12:10:26 +0700

    demo

--- WebBanSua/Models/CuaHangBanSuaContext.cs
+++ WebBanSua/Models/CuaHangBanSuaContext.cs
@@ -33,7 +33,7 @@ namespace WebBanSua.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DUNG10NGON\\SQLEXPRESS;Database=CuaHangBanSua;Integrated security=true;");
+                optionsBuilder.UseSqlServer("Server=TranDucHai;Database=CuaHangBanSua;Integrated Security=true;");
             }
         }
 
